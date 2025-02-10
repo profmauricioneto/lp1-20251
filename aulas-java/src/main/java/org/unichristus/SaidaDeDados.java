1885e5e98bce459864f7c1f70994e4ad0e6cfb25
profmauricioneto <mauricio.moreira@unicristus.edu.br>
2025-02-10 20:34:23 -0300

    add: primeira aula de java

---
+++ aulas-java/src/main/java/org/unichristus/SaidaDeDados.java
@@ -0,0 +1,14 @@
+package org.unichristus;
+
+public class SaidaDeDados {
+    public static void main(String[] args) {
+        String name = "Mauricio Neto";
+        int age = 35;
+        boolean isProfessor = true;
+
+        System.out.print("Nome: " + name + "\n");
+        System.out.println("Idade: " + age);
+        System.out.println("É professor? " + isProfessor);
+        System.out.printf("Idade: %d", age);
+    }
+}
